09e9fb0b9c6f3a71e7cae708546f3e94ee6ee5bd
Your Name <you@example.com>
2022-05-22 05:57:12 +0600

    initializion and mongo connection done

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njevu.mongodb.net/?retryWrites=true&w=majority`;
+
+app.get('/', (req, res) => {
+    res.send('Hello From my manufacture website')
+})
+
+app.listen(port, () => {
+    console.log(`this website is running on port ${port}`)
+})
