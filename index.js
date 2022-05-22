077a4c4607cc26cff7ef2b13bb4e1986d6ebb5f4
Your Name <you@example.com>
2022-05-22 16:51:03 +0600

    single id api creted

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -27,6 +27,13 @@ async function run() {
         app.get('/review', async (req, res) => {
             const result = await reviewCollection.find().toArray()
             res.send(result)
+        })
+
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const laptop = await toolCollection.findOne(query)
+            res.send(laptop)
         })
     }
     finally {
