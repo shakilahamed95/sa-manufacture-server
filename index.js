809dd89988708a421ff9a7c0286bf6a88628f181
Your Name <you@example.com>
2022-05-24 04:38:44 +0600

    add comments route created

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -82,6 +83,12 @@ async function run() {
             const result = await orderCollection.find(query).toArray()
             res.send(result)
         })
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const quary = { _id: ObjectId(id) }
+            const result = await orderCollection.findOne(quary)
+            res.send(result)
+        })
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -129,6 +136,23 @@ async function run() {
             const result = await orderCollection.deleteOne(filter)
             res.send(result)
         })
+        app.post('/create-payment-intent', async (req, res) => {
+            const tool = req.body;
+            const price = tool.totalMoney;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
+        app.post('/review', async (req, res) => {
+            const comment = req.body;
+            const result = await reviewCollection.insertOne(comment)
+            res.send(result)
+        })
+
     }
     finally {
 
