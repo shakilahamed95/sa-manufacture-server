962df196faeab93b00c66df885ba839e4e17eb59
Your Name <you@example.com>
2022-05-26 05:30:09 +0600

    update done

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ async function run() {
             const result = await orderCollection.findOne(quary)
             res.send(result)
         })
-        app.put('/user/:email', verifyJWT, async (req, res) => {
+        app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
             const filter = { email: email };
